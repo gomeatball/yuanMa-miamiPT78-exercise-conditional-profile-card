1d65e167852290df4eafe7d69ac790bb7663315c
Yuan Ma <mayuan131522@gmail.com>
2025-04-03 23:48:24 +0000

    main

--- src/js/app.js
+++ src/js/app.js
@@ -41,10 +41,40 @@ function render(variables = {}) {
           <h3>${variables.city === null ? "city" : variables.city}, 
           ${variables.country === null ? "country" : variables.country}</h3>
           <ul class=${variables.socialMediaPosition}>
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          ${
+            variables.twitter !== null
+              ? `<li>
+                <a href=${variables.twitter}>
+                <i class="fab fa-twitter"></i></a>
+              </li>`
+              : ""
+          }
+           ${
+             variables.github !== null
+               ? `<li>
+                <a href=${variables.github}>
+               <i class="fab fa-github"></i></a>
+              </li>`
+               : ""
+           }
+            ${
+              variables.linkedin !== null
+                ? `<li>
+               <i class="fab fa-linkedin"><a href=${variables.linkedin}>
+               </i></a>
+              </li>`
+                : ""
+            }
+            ${
+              variables.instagram !== null
+                ? `<li>
+               <a href=${variables.instagram}>
+              <i class="fab fa-instagram"></i></a>
+              </li>`
+                : ""
+            }
+            
+           
           </ul>
         </div>
     `;
